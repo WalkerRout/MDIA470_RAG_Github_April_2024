642fd245d3576cd7a99fb4e253a5ef888776743f
walkerrout04@gmail.com <walkerrout04@gmail.com>
2024-01-30 19:03:53 -0800

    Comment and readme update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # project3
-### TODO
+### To Run
+- Clone this repository
+	- `git clone https://github.com/WalkerRout/project3.git`
+- Navigate into the project
+	- `cd project3/`
+- Ensure the docker daemon is installed and running (simply install and run docker desktop https://docs.docker.com/get-docker/)
+- Run the project
+	- `docker compose up`
+- Open `http://localhost:5000/` in browser
+> Stop the project with: `docker compose stop`
+> 
+> Remove docker generated images/containers/caches: `docker system prune`
--- app/app.py
+++ app/app.py
@@ -3,29 +3,49 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
 from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-  return "<h1>Booyah flask and docker!</h2>"
+  """
+  @params:  N/A
+  @purpose: present root template
+  @returns: str - HTML presented to user
+  """
+
+  return r"""
+    <a href="/ollama"><h1>Ollama Model</h2></a>
+  """
 
 @app.route("/ollama", methods=("get", "post"))
 def ollama():
+  """
+  @params:  N/A
+  @purpose: accept query string with argument `topic`, feed topic into langchain chain
+            using Ollama as a model
+  @returns: str - HTML presented to user
+  """
+
   topic = request.args.get("topic")
+
   if topic is not None:
-    llm = Ollama(base_url="http://ollama:11434", model="mistral")
+    # construct chain using prompt and ollama instance running on other container
     prompt = ChatPromptTemplate.from_template("Tell me a short joke about {topic}")
+    llm = Ollama(base_url="http://ollama:11434", model="mistral")
     chain = prompt | llm | StrOutputParser()
+
     answer = chain.invoke({"topic": topic})
     return f"Topic: {topic}<br>Joke: {answer}"
   else:
     return r"""
-<h1> Enter topic </h1>
-<form action="/ollama">
-  <input name="topic" value="Bears" />
-  <input type="submit" /> 
-</form>
-"""
+      <h1> Enter topic </h1>
+      <form action="/ollama">
+        <input name="topic" value="Bears" />
+        <input type="submit" />
+      </form>
+    """
 
 if __name__ == "__main__":
+  # run with debug=True for hot reloading
   app.run(debug=True)
